b1ccbf03d22ca7ee1c8a2807f9c0497d8be6de4e
dfchang149 <dfchang@ucsd.edu>
2022-09-04 07:54:03 -0700

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace Fluent_Tic_tac_toe;
+/// <summary>
+/// An empty window that can be used on its own or navigated to within a Frame.
+/// </summary>
+public sealed partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        this.InitializeComponent();
+        ExtendsContentIntoTitleBar = true;
+        SetTitleBar(AppTitleBar);
+    }
+
+    private void PlaySingleplayerClick(object sender, RoutedEventArgs e)
+    {
+        
+        ContentFrame.Navigate(typeof(Pages.PlayingPage));
+        PlayButton.Visibility = Visibility.Collapsed;
+        SettingsButton.Visibility = Visibility.Collapsed;
+    }
+
+    private void PlayMultiplayerClick(object sender, RoutedEventArgs e)
+    {
+        ContentFrame.Navigate(typeof(Pages.PlayingPage));
+    }
+
+    private void SettingsButtonClick(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void WindowActivated(object sender, WindowActivatedEventArgs e)
+    {
+        if (e.WindowActivationState == WindowActivationState.Deactivated)
+        {
+            TitleBarFill.Visibility = Visibility.Collapsed;
+        }
+        else
+        {
+            TitleBarFill.Visibility = Visibility.Visible;
+        }
+    }
+}
+
---
+++ Pages/PlayingPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace Fluent_Tic_tac_toe.Pages;
+/// <summary>
+/// An empty page that can be used on its own or navigated to within a Frame.
+/// </summary>
+public sealed partial class PlayingPage : Page
+{   
+    public PlayingPage()
+    {
+        this.InitializeComponent();
+        this.CreateBoard();
+    }
+
+    private void CreateBoard()
+    {
+        for (var r = 0; r < 3; r++)
+        {
+            for(var c = 0; c < 3; c++)
+            {
+                var index = (r * 3) + c;
+                var button = new Button();
+                button.Height = 100;
+                button.Width = 100;
+                button.Content = "";
+                button.Name = "square" + index;
+
+                Grid.SetRow(button, r % 3);
+                Grid.SetColumn(button, c % 3);
+                Board.Children.Add(button);
+            }
+        }
+    }
+
+    private void PlacePiece()
+    {
+        
+    }
+}
