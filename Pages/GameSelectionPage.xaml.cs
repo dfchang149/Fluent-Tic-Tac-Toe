4b5b18e648a478b6469af6e43e4d5b60b03fe143
dfchang149 <dfchang@ucsd.edu>
2022-09-11 16:38:45 -0700

    Added more settings and an option to change App Theme, but it doesn't work yet

--- Pages/GameSelectionPage.xaml.cs
+++ Pages/GameSelectionPage.xaml.cs
@@ -12,6 +12,7 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Graphics.Printing3D;
 using Windows.Security.Isolation;
 
 // To learn more about WinUI, the WinUI project structure,
@@ -27,6 +28,27 @@ public sealed partial class GameSelectionPage : Page
     {
         this.InitializeComponent();
         GamemodeExpander.Content = null;
+    }
+
+    private void ThemeSelected(object sender, RoutedEventArgs e)
+    {
+        switch (ThemeSelectionBox.SelectedIndex)
+        {
+            case 0:
+                this.RequestedTheme = ElementTheme.Light;
+                break;
+            case 1:
+                this.RequestedTheme = ElementTheme.Dark;
+                break;
+            case 2:
+                this.RequestedTheme = ElementTheme.Default;
+                break;
+            default:
+                ThemeSelectionBox.SelectedIndex = 2;
+                this.RequestedTheme = ElementTheme.Default;
+                break;
+        }
+        Settings.theme = ThemeSelectionBox.SelectedIndex;
     }
 
     private void UpdateGamemodeExanderContent()
@@ -82,19 +104,45 @@ public sealed partial class GameSelectionPage : Page
         }
     }
 
+    private void UpdatePlayerBoxes()
+    {
+        if(MultiplayerPlayersBox != null && MultiplayerBotsBox != null && SpectatorBotsBox != null && MaxPlayersMultiplayerText != null && MaxPlayersSpectatorText != null)
+        {
+            var MaxPlayers = Settings.GetMaxPlayers();
+            MaxPlayersMultiplayerText.Text = MaxPlayers.ToString();
+            MaxPlayersSpectatorText.Text = MaxPlayers.ToString();
+            MultiplayerPlayersBox.Maximum = MaxPlayers;
+            MultiplayerBotsBox.Maximum = MaxPlayers - MultiplayerPlayersBox.Value;
+            MultiplayerPlayersBox.Maximum = MaxPlayers - MultiplayerBotsBox.Value;
+            SpectatorBotsBox.Maximum = MaxPlayers;
+
+            if (Settings.gamemode.Equals(Settings.gamemodes[1]))
+            {
+                Settings.numPlayers = (int)MultiplayerPlayersBox.Value;
+                Settings.numMultiplayerBots = (int)MultiplayerBotsBox.Value;
+            }
+            else if(Settings.gamemode.Equals(Settings.gamemodes[2]))
+            {
+                Settings.numSpectatorBots = (int)SpectatorBotsBox.Value;
+            }
+        }
+    }
+
     private void BoardRowsChanged(NumberBox sender, NumberBoxValueChangedEventArgs e)
     {
-
+        Settings.boardSize.Y = (float) e.NewValue;
+        UpdatePlayerBoxes();
     }
 
     private void BoardColumnsChanged(NumberBox sender, NumberBoxValueChangedEventArgs e)
     {
-
+        Settings.boardSize.X = (float)e.NewValue;
+        UpdatePlayerBoxes();
     }
 
     private void WinPatternChanged(object sender, RoutedEventArgs e)
     {
-
+        Settings.winPattern = WinPatternSelectionBox.SelectedIndex;
     }
 
     private void BoardExanderExpanded(Expander sender, ExpanderExpandingEventArgs e)
@@ -109,19 +157,36 @@ public sealed partial class GameSelectionPage : Page
 
     private void PlayerBoxChanged(NumberBox sender, NumberBoxValueChangedEventArgs e)
     {
-
+        UpdatePlayerBoxes();
     }
 
     private void BotsBoxChanged(NumberBox sender, NumberBoxValueChangedEventArgs e)
     {
-
+        UpdatePlayerBoxes();
     }
 
+    private void TimerToggled(object sender, RoutedEventArgs e)
+    {
+        Settings.matchTimerEnabled = TimerToggleSwitch.IsOn;
+    }
+
+    private void SquaresInfoToggled(object sender, RoutedEventArgs e)
+    {
+        Settings.boardInfoEnabled = SquaresInfoToggleSwitch.IsOn;
+    }
+
+    private void PlayerCounterToggled(object sender, RoutedEventArgs e)
+    {
+        Settings.playerCounterEnabled = PlayerCounterToggleSwitch.IsOn;
+    }
 
     private void ResetGameSettingsClick(object sender, RoutedEventArgs e)
     {
         ResetGameSettingsButton.Flyout.Hide();
         Settings.Reset();
+
+        ThemeSelectionBox.SelectedIndex = Settings.theme;
+
         GamemodeSelectionBox.SelectedIndex = Settings.gamemode;
         BoardSelectionBox.SelectedIndex = Settings.boardMode;
         MultiplayerPlayersBox.Value = Settings.numPlayers;
@@ -130,24 +195,9 @@ public sealed partial class GameSelectionPage : Page
         DifficultySelectionBox.SelectedIndex = Settings.difficulty;
         BoardRowSelection.Value = Settings.boardSize.Y;
         BoardColumnSelection.Value = Settings.boardSize.X;
+        WinPatternSelectionBox.SelectedIndex = Settings.winPattern;
         TimerToggleSwitch.IsOn = Settings.matchTimerEnabled;
         SquaresInfoToggleSwitch.IsOn = Settings.boardInfoEnabled;
         PlayerCounterToggleSwitch.IsOn = Settings.playerCounterEnabled;
-
-    }
-
-    private void TimerToggled(object sender, RoutedEventArgs e)
-    {
-
-    }
-
-    private void SquaresInfoToggled(object sender, RoutedEventArgs e)
-    {
-
-    }
-
-    private void PlayerCounterToggled(object sender, RoutedEventArgs e)
-    {
-
     }
 }
--- Settings.cs
+++ Settings.cs
@@ -17,6 +17,7 @@ internal class Settings
 
     public static List<Player> players = new();
     public static int numPlayers = 0;
+    public static int numSingleplayerBots = 0;
     public static int numMultiplayerBots = 0;
     public static int numSpectatorBots = 2;
 
@@ -31,11 +32,19 @@ internal class Settings
     public static bool boardInfoEnabled = true;
     public static bool playerCounterEnabled = true;
 
+    public static bool informedMaxPlayers = false;
+
+    public static string[] themes = {"Light","Dark","Use System Settings" };
+    public static int theme = 2;
+
     public static void Reset()
     {
+        theme = 2;
+        
         gamemode = 0;
         boardMode = 0;
         numPlayers = 0;
+        numSingleplayerBots = 1;
         numMultiplayerBots = 0; 
         numSpectatorBots = 2;
         difficulty = 1;
@@ -44,5 +53,10 @@ internal class Settings
         matchTimerEnabled = true;
         boardInfoEnabled = true;
         playerCounterEnabled = true;
+    }
+
+    public static int GetMaxPlayers() // returns max amount of players for current board size
+    {
+        return (int) Math.Round((boardSize.X * boardSize.Y)/4.5f);
     }
 }
